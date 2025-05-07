fe92e8b0ea55d7b06f111942b8b7f894cf708a91
Korneliusz Buczkowski <buczkowskikorneliusz@gmail.com>
2025-05-07 10:55:19 +0200

    add offboard component as offboard view

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import {Routes} from '@angular/router';
 import {EmployeesComponent} from './employees/employees.component';
 import {EmployeeComponent} from './employee/employee.component';
+import {OffboardComponent} from './offboard/offboard.component';
 
 export const routes: Routes = [
   {path: 'employees', component: EmployeesComponent,},
   {path: 'employees/:id', component: EmployeeComponent},
+  {path: 'employees/:id/offboard', component: OffboardComponent},
   {path: '**', redirectTo: 'employees'}
 ];
---
+++ src/app/offboard/offboard.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-offboard',
+  imports: [],
+  templateUrl: './offboard.component.html',
+  styleUrl: './offboard.component.css'
+})
+export class OffboardComponent {
+
+}
