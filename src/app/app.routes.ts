a14973f9292669539414359c6a3741fd0c9e783a
Korneliusz Buczkowski <buczkowskikorneliusz@gmail.com>
2025-05-07 10:54:26 +0200

    add employee component as details view

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import {Routes} from '@angular/router';
 import {EmployeesComponent} from './employees/employees.component';
+import {EmployeeComponent} from './employee/employee.component';
 
 export const routes: Routes = [
-  {path: 'employees', component: EmployeesComponent},
+  {path: 'employees', component: EmployeesComponent,},
+  {path: 'employees/:id', component: EmployeeComponent},
   {path: '**', redirectTo: 'employees'}
 ];
