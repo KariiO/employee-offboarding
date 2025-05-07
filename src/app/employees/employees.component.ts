165b728e17206daf54b82ce02d4d10a10224438b
Korneliusz Buczkowski <buczkowskikorneliusz@gmail.com>
2025-05-07 15:57:37 +0200

    add search functionality

--- src/app/employees/employees.component.ts
+++ src/app/employees/employees.component.ts
@@ -1,15 +1,17 @@
-import {Component, inject} from '@angular/core';
+import {Component, computed, inject, signal} from '@angular/core';
 import {RouterLink} from '@angular/router';
 import {EquipmentService} from '../api/employee.service';
 import {TitleCasePipe} from '@angular/common';
 import {FriendlyEquipmentsPipe} from '../pipes/friendly-equipments.pipe';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-employees',
   imports: [
     RouterLink,
     TitleCasePipe,
-    FriendlyEquipmentsPipe
+    FriendlyEquipmentsPipe,
+    FormsModule
   ],
   templateUrl: './employees.component.html',
   styleUrl: './employees.component.css'
@@ -18,4 +20,12 @@ export class EmployeesComponent {
   private readonly _equipmentService = inject(EquipmentService);
 
   employeesResource = this._equipmentService.getAll();
+  searchTerm = signal<string>('');
+  employees = computed(() => {
+    const searchTerm = this.searchTerm().toLowerCase();
+
+    return this.employeesResource.value()?.filter(employee => {
+      return employee.name.toLowerCase().includes(searchTerm) || employee.department.toLowerCase().includes(searchTerm);
+    })
+  })
 }
